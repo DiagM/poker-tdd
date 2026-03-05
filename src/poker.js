1ed3cc76283d3d344adc2182f12d89ce1cafd99e
Mohamed DIAG <Mohamed.DIAG.dz@viacesi.fr>
2026-03-05 15:18:05 +0100

    feat: implement card parser

--- src/poker.js
+++ src/poker.js
@@ -1,6 +1,26 @@
-export function evaluateHand(cards) {
+const rankMap = {
+  "2":2,"3":3,"4":4,"5":5,"6":6,
+  "7":7,"8":8,"9":9,"10":10,
+  "J":11,"Q":12,"K":13,"A":14
+}
+
+export function parseCard(card){
+
+  const suit = card.slice(-1)
+  const rankStr = card.slice(0,-1)
+
   return {
-    category: "High Card",
-    chosen5: [...cards],
-  };
+    rank: rankMap[rankStr],
+    suit
+  }
+
+}
+
+export function evaluateHand(cards){
+
+  return {
+    category:"High Card",
+    chosen5:[...cards]
+  }
+
 }
